daee5c028c198e679204173eece8eb88272756c8
Marek M <kontodowszystkiego@o2.pl>
2014-03-08 22:27:24 +0100

    MainViewController and StoryBoards added

---
+++ SimpleTaskManager/MainView/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SimpleTaskManager
+//
+//  Created by Marek M on 08.03.2014.
+//  Copyright (c) 2014 Tomato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ SimpleTaskManager/MainView/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  SimpleTaskManager
+//
+//  Created by Marek M on 08.03.2014.
+//  Copyright (c) 2014 Tomato. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

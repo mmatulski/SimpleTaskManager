c29459130aac798d3c2d2b13ad5bf3fe2183e5fa
Marek M <kontodowszystkiego@o2.pl>
2014-03-16 15:05:18 +0100

    TaskCompleteButton added

---
+++ SimpleTaskManager/MainView/DialogsPresentation/Buttons/TaskCompleteButton.h
@@ -0,0 +1,11 @@
+//
+// Created by Marek M on 16.03.2014.
+// Copyright (c) 2014 Tomato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MiniHintView.h"
+
+
+@interface TaskCompleteButton : MiniHintView
+@end
---
+++ SimpleTaskManager/MainView/DialogsPresentation/Buttons/TaskCompleteButton.m
@@ -0,0 +1,12 @@
+//
+// Created by Marek M on 16.03.2014.
+// Copyright (c) 2014 Tomato. All rights reserved.
+//
+
+#import "TaskCompleteButton.h"
+
+
+@implementation TaskCompleteButton {
+
+}
+@end
